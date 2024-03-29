5afb80e1b385b31b5ceb54770316653f80e48755
i-sanjay-cs <sanjay826870@gmail.com>
2024-03-29 10:20:57 +0530

    Added flask file and requirements fiole to be used for the jenskins ci/cd operation

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import time
 
 app = Flask(__name__)
 
@@ -7,4 +8,13 @@ def hello():
     return 'Hello, World!'
 
 if __name__ == '__main__':
+    # Run the Flask app in debug mode
     app.run(debug=True)
+    
+    # Delay execution for 5 seconds
+    time.sleep(5)
+    # After 5 seconds, stop the Flask app
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
