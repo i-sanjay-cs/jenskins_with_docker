dc90f383e994e801b831047aa7388f3955349ddb
i-sanjay-cs <sanjay826870@gmail.com>
2024-03-29 10:24:17 +0530

    Added flask file and requirements fiole to be used for the jenskins ci/cd operation

--- app.py
+++ app.py
@@ -1,20 +1,30 @@
-from flask import Flask
-import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-app = Flask(__name__)
+# Set ChromeDriver path
+chrome_driver_path = r"C:\\Users\\sanja\\Downloads\\chromedriver-win64 (1)\\chromedriver.exe"
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+# Create a new instance of the Chrome driver
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
-if __name__ == '__main__':
-    # Run the Flask app in debug mode
-    app.run(debug=True)
-    
-    # Delay execution for 5 seconds
-    time.sleep(5)
-    # After 5 seconds, stop the Flask app
-    func = request.environ.get('werkzeug.server.shutdown')
-    if func is None:
-        raise RuntimeError('Not running with the Werkzeug Server')
-    func()
+# Navigate to the Saucedemo login page
+driver.get("https://www.saucedemo.com/")
+
+# Enter username and password
+username_input = driver.find_element(By.ID, "user-name")
+password_input = driver.find_element(By.ID, "password")
+username_input.send_keys("standard_user")
+password_input.send_keys("secret_sauce")
+
+# Click the login button
+login_button = driver.find_element(By.ID, "login-button")
+login_button.click()
+
+# Verify if login is successful
+if "inventory.html" in driver.current_url:
+    print("Login Successful!")
+else:
+    print("Login Failed!")
+
+# Close the browser
+driver.quit()
